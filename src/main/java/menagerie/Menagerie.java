8ccb19c477395bfbfaea0ada7b34d94c3db9794d
Jacob <hayesj1@users.noreply.github.com>
2017-10-24 11:54:01 -0400

    Exlcuded the .idea directory

---
+++ src/main/java/menagerie/Menagerie.java
@@ -0,0 +1,10 @@
+package main.java.menagerie;
+
+import com.jme3.app.SimpleApplication;
+
+public class Menagerie extends SimpleApplication{
+    @Override
+    public void simpleInitApp() {
+
+    }
+}
